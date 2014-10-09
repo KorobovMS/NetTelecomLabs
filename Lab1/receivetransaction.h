b4f88eb5c19088b1291e802e2d6c9e76325e2799
LenaBuranova <alenaburanova.93@gmail.com>
2014-10-09 13:47:04 +0300

    Update receivetransaction.h

--- Lab1/receivetransaction.h
+++ Lab1/receivetransaction.h
@@ -16,30 +16,25 @@ class ReceiveTransaction : public QObject
     Q_OBJECT
 
 public:
-    explicit ReceiveTransaction( QHostAddress addr, quint16 port );
+    ReceiveTransaction( QHostAddress addr, quint16 port );
 
 signals:
     void StartReceiving();
     void Progress(int bytes_received, int bytes_total);
-    void FinishSending();
-
+    void FinishReceiving();
 private slots:
     void PendingMessage();
-
 private:
-
     void SendMessage( quint32 state, quint32 id,  quint32 seq );
     void LoadFile( Message& msg  );
-    bool ReceiveMessage( );
+    void  ReceiveMessage( );
     void RegIdStore( Message& msg );
     void DelId( Message& msg );
     void RegId( Message& msg );
     void SendFinish( Message& msg );
 
-
     QMap < quint32, QString >  file_for_id_;
     QMap < quint32, quint32 > last_seq_for_id_;
-    //QString path_;
     QUdpSocket socket_;
     QHostAddress addr_;
     quint16 port_;
